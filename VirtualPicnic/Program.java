c811cc1774e1d7eb9470f6a2aa2c1430b9f9bdc1
Totalpolarfox <totalpolarfox@outlook.com>
2024-04-15 18:13:40 +0300

    Added try-catch

--- VirtualPicnic/Program.java
+++ VirtualPicnic/Program.java
@@ -1,5 +1,20 @@
 package VirtualPicnic;
 
+import java.io.IOException;
+import java.util.List;
+
 public class Program {
+
+    public static void main(String[] args) throws IOException {
+        
+        try {
+            List<String> words = GetData.readTextFromFile("input.txt");   
+            
+            
+        } catch (IOException e) {
+            // TODO: handle exception
+        }
+    }
+    
 
 }
