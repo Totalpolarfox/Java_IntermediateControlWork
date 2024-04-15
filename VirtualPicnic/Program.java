c9d994f0f6ea2df733266cd0d8e91671b93a6b35
Totalpolarfox <totalpolarfox@outlook.com>
2024-04-15 18:22:18 +0300

    Added instructions

--- VirtualPicnic/Program.java
+++ VirtualPicnic/Program.java
@@ -2,6 +2,7 @@ package VirtualPicnic;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.TreeMap;
 
 public class Program {
 
@@ -9,7 +10,10 @@ public class Program {
         
         try {
             List<String> words = GetData.readTextFromFile("input.txt");   
-            
+            TreeMap<String, Integer> wordCountMap = (TreeMap<String, Integer>) TextProcessor.countWords(words);
+            String longestWord = TextProcessor.findLongestWord(words);
+            System.out.println("Количество слов: " + wordCountMap.size());
+            System.out.println("Самое длинное слово: " + longestWord);
             
         } catch (IOException e) {
             // TODO: handle exception
